f5ee3bfa4a210d4f06379dc8e5f18a0a7bcc0cbe
Cole Winchester <cowinch@gmail.com>
2023-06-19 23:43:22 -0500

    added analog circle

--- clock.js
+++ clock.js
@@ -3,19 +3,24 @@ let minutes = document.getElementById('minutes');
 let seconds = document.getElementById('seconds');
 let ampm = document.getElementById('ampm');
 
-const interval = setInterval(timer, 1000);
+let hh = document.getElementById("hh");
+let mm = document.getElementById("mm");
+let ss = document.getElementById("ss");
+
+const interval = setInterval(timer, 100);
 function timer() {
     let h = new Date().getHours();
     let m = new Date().getMinutes();
     let s = new Date().getSeconds();
     let am = h >= 12 ? "PM" : "AM";
 
-    console.log("interval")
+    // console.log("interval")
     //convert between 24hr and 12hr clock
     if (h > 12) {
         h = h - 12;
     }
 
+    //adds a zero so the numbers are always double digit
     h = (h < 10) ? "0" + h : h;
     m = (m < 10) ? "0" + m : m;
     s = (s < 10) ? "0" + s : s;
@@ -24,5 +29,9 @@ function timer() {
     minutes.innerHTML = m;
     seconds.innerHTML = s;
     ampm.innerHTML = am;
+
+    hh.style.strokeDashoffset = 440 - (440 * h) / 12;
+    mm.style.strokeDashoffset = 440 - (440 * m) / 60;
+    ss.style.strokeDashoffset = 440 - (440 * s) / 60;
 }
 
